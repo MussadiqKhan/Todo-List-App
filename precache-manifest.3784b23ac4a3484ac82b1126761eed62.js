aa042a1ae41e0903cb0b3f720d5212a52c4dc7aa
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2019-08-07 23:22:46 +0500

    Updates

---
+++ precache-manifest.3784b23ac4a3484ac82b1126761eed62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974d765149bffc0b0d8f3ff8006b5c23",
+    "url": "/Todo-List-App/index.html"
+  },
+  {
+    "revision": "277bb6dd133471c954fc",
+    "url": "/Todo-List-App/static/css/main.bd3880ac.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/Todo-List-App/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "277bb6dd133471c954fc",
+    "url": "/Todo-List-App/static/js/main.8da8760e.chunk.js"
+  },
+  {
+    "revision": "390ca683b95fb2a0d7aa",
+    "url": "/Todo-List-App/static/js/runtime~main.1f6feaad.js"
+  }
+]);
